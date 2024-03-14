6bfd6c5f27083346a32b4a913f14f9511eda5572
Free simulation software <26973313+opensimply@users.noreply.github.com>
2024-03-14 09:18:01 +0100

    Update MMCqueue.pas

--- MMCqueue.pas
+++ MMCqueue.pas
@@ -10,45 +10,46 @@
 program MMCqueue;
 {$apptype GUI}  
 {$S-}
+
 uses
   SimBase,
   SimBlocks,
   SimStdGUI;
   
 type
-  TMyModel = class(TModel)             //  Model declaration.  
+  TMyModel = class(TModel)                        //  Model declaration.  
     procedure Body; override;
   end;
                                      
 var                                   
-  Capacity,                            //  Model parameters.
-  NumberOfServers: integer;             
+  Capacity,                                        //  Model parameters.
+  NumberOfServers: Integer;             
   InterarrivalTime,                     
   ServiceTime: Double;                  
  
-procedure TMyModel.Body;               //  Model description.
+procedure TMyModel.Body;                           //  Model description.
 var                                    
-  Gen: TGenerator;                     //  Blocks declaration.
+  Gen: TGenerator;                                 //  Blocks declaration.
   Que: TQueue;   
   Sel: TSelector;
 begin 
-  Gen:=TGenerator.Create([Capacity,ExpTime,InterarrivalTime]);
-  Que:=TQueue.CreateDefault;
-  Sel:=TSelector.Create([NumberOfServers]);
+  Gen := TGenerator.Create([Capacity, ExpTime, InterarrivalTime]);
+  Que := TQueue.CreateDefault;
+  Sel := TSelector.Create([NumberOfServers]);
   Gen.Connect(Que);
   Que.Connect(Sel);
-  Sel.Connect(1,NumberOfServers,TServer,[ExpTime,ServiceTime]); 
+  Sel.Connect(1, NumberOfServers, TServer, [ExpTime, ServiceTime]); 
   
-  Run(Gen);                            //  Launching the start block.
+  Run(Gen);                                        //  Launching the start block.
                                        
-  Que.ShowStat;                        //  Results.  
+  Que.ShowStat;                                    //  Results.  
 end;
 
 begin   
-  Capacity:=3000;                      //  Initial values of the model.
-  InterarrivalTime:=1;                 
-  ServiceTime:=1.3;                    
-  NumberOfServers:=2;                  
+  Capacity := 3000;                                //  Initial values of the model.
+  InterarrivalTime := 1;                 
+  ServiceTime := 1.3;                    
+  NumberOfServers := 2;                  
 
-  Simulate(TMyModel,'The M/M/C queuing system');     //  Starting a model.
+  Simulate(TMyModel, 'The M/M/C queuing system');  //  Starting a model.
 end.
